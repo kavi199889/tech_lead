eaa517d37f528131b93e61c8521f2cda28ba1929
hp <kavi199889>
2020-10-05 00:56:04 +0530

    payment

---
+++ app/src/test/java/MainActivity.java
@@ -0,0 +1,6 @@
+import com.braintreepayments.cardform.view.CardForm;
+
+public class MainActivity {
+    CardForm cardForm = findViewById(R.id.card_form);
+    Button buy = findViewById(R.id.btnBuy);
+}
