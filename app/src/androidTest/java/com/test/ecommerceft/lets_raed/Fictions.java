e75280f0c6b11b6e2e66c0e572d85403a1e00d69
hp <kavi199889>
2020-10-08 13:09:44 +0530

    Add to cart(IT19135144)

---
+++ app/src/androidTest/java/com/test/ecommerceft/lets_raed/Fictions.java
@@ -0,0 +1,14 @@
+package com.test.ecommerceft.lets_raed;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Fictions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fictions);
+    }
+}
